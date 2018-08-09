f3deabece3663fa64a8160588c6b9647665ab608
ramprit <Rampritsahani@outlook.in>
2018-08-09 19:53:16 +0530

    add new users

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const mongooseUniqueValidator = require("mongoose-unique-validator");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const userSchema = mongoose.Schema({
   email: {
@@ -9,5 +9,5 @@ const userSchema = mongoose.Schema({
   },
   password: { type: String, require: true }
 });
-userSchema.plugin(mongooseUniqueValidator);
+userSchema.plugin(uniqueValidator);
 module.exports = mongoose.model("user", userSchema);
